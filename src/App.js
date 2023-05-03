e418509ce1b159347935ca0f8d3983d1a6aadfb1
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2023-05-03 10:00:04 +0200

    until Constext

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+//main app component
+import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
+import Header from './component/Header'
+import FeedbackList from './component/FeedbackList'
+import FeedbackStats from './component/FeebackStats'
+import FeedbackForm from './component/FeedbackForm'
+import AboutIconLink from './component/AboutIconLink'
+import AboutPage from './pages/AboutPage'
+import { FeedbackProvider } from './component/context/feedbackContext'
+import FeedbackData from './data/feedbackData'
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData) //app level state
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id)) //Här filterar vi bort the Item med samma id som i parametern.
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                  />
+                </>
+              }
+            ></Route>
+
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
+  )
 }
 
-export default App;
+export default App
